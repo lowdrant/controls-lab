3e152711a3836635277dcf86a49302663e0616c9
Marion Anderson <lmanderson42@gmail.com>
2018-08-27 21:22:20 -0400

    Completed Lab1 and added more PDF documentation

---
+++ Lab1/Task1/main.c
@@ -0,0 +1,34 @@
+/**
+ * Task 1 for ECE4550 Fall 2018
+ *
+ * Author: Marion Anderson
+ * Date: 2018-08-27
+ * File: main.c
+ */
+#include "F2837xD_device.h"
+
+unsigned long slow = 0, fast = 0;  // counters to view program execution
+
+int main(void)
+{
+    // Turn Watchdog off & back on, per lab instructions
+    // 'off' bits: 00000000 | 0 | 1 | 101 | 000
+    // 'on' bits:  00000000 | 0 | 0 | 101 | 000
+    //
+    // Bits 6 thru 3 are the important ones.
+    //     bit 6 turns watchdog on (0) and off (1)
+    //     bits 5-3 are an error check. Must be '101' every time WDCR is written. Else reset
+    WdRegs.WDCR.all = 0x68;
+    WdRegs.WDCR.all = 0x28;
+
+	while (1) {
+	    fast++;  // increment fast every loop
+	    if (fast % 100000 == 0) {  // increment slow once every 10^5 loops
+	        slow++;
+	    }
+
+	    // Reset watchdog constantly
+	    WdRegs.WDKEY.all = 0x55;
+	    WdRegs.WDKEY.all = 0xAA;
+	}
+}
---
+++ Lab1/Task2/main.c
@@ -0,0 +1,49 @@
+/**
+ * Task 2 for ECE4550 Fall 2018
+ *
+ * Author: Marion Anderson
+ * Date: 2018-08-27
+ * File: main.c
+ */
+#include "F2837xD_device.h"
+#include "math.h"
+#ifndef PI
+#define PI 3.14159
+#endif
+
+unsigned long slow = 0, fast = 0;  // counters to view program execution
+float x[5000], y[5000];            // math coordinate arrays
+
+
+int main(void)
+{
+    // Turn Watchdog off & back on, per lab instructions
+    // 'off' bits: 00000000 | 0 | 1 | 101 | 000
+    // 'on' bits:  00000000 | 0 | 0 | 101 | 000
+    //
+    // Bits 6 thru 3 are the important ones.
+    //     bit 6 turns watchdog on (0) and off (1)
+    //     bits 5-3 are an error check. Must be '101' every time WDCR is written. Else reset
+    WdRegs.WDCR.all = 0x68;
+    WdRegs.WDCR.all = 0x28;
+
+    while (1) {
+        // Compute x & y arrays
+        // use fast as index to save memory
+        if (fast < 5000) {
+            x[fast] = (float) fast / 5000;
+            y[fast] = cos(2 * PI * x);
+        }
+
+        // Counter incrementation
+        // not taking advantage of operator precedence for readability
+        fast++;
+        if (fast % 100000 == 0) {
+            slow++;
+        }
+
+        // Reset watchdog constantly
+        WdRegs.WDKEY.all = 0x55;
+        WdRegs.WDKEY.all = 0xAA;
+    }
+}
