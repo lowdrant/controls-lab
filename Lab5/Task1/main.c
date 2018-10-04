127309a2ed812882c1e461a59d5ab046a6a319a0
Marion Anderson <lmanderson42@gmail.com>
2018-10-04 14:51:57 -0400

    Updated main file

--- Lab5/Task1/main.c
+++ Lab5/Task1/main.c
@@ -6,7 +6,6 @@
  */
 
 #include "F2837xD_device.h"
-#include "F2837xD_pievect.h"
 #include "math.h"
 #ifndef PI
 #define PI 3.1415
@@ -15,8 +14,11 @@
 #define VREF 3
 #endif
 
-float theta;  // motor initial position
-float vavg;   // desired average voltage on PWM
+//float theta;  // motor initial position
+float vavg = 20;        // desired average voltage on PWM
+Uint16 state = 0;       // state tracker
+Uint32 tmr0_count = 0;  // software "clock divider" for switching state
+Uint32 ct = 0;
 interrupt void vcalcISR(void);
 
 
@@ -43,25 +45,52 @@ int main(void)
     ClkCfgRegs.SYSCLKDIVSEL.all = 0;              // final clock division
 
     // PWM
-    // fpwm = 100MHz
+    // fclk,pwm = 100MHz
+    // fpwm = 25kHz
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO0 = 0;
+    GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1;
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO1 = 0;
+    GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;
     ClkCfgRegs.PERCLKDIVSEL.bit.EPWMCLKDIV = 1;  // 200MHz / 2 = 100MHz
-    CpuSysRegs.PCLKCR2.bit.EPWM? = 1;            // module clk EN
+    CpuSysRegs.PCLKCR2.bit.EPWM1 = 1;            // module clk EN
     asm(" NOP"); asm(" NOP");                    // critical wait
-    // TODO: use xbar? (p. 1682)
-    EPwm?Regs.TBCTL.bit.CTRMODE = 0;  // up-count mode
-    EPwm?Regs.TBPRD = 2000;           // 2VDC/TBPRD = vres = 24mV = 48V/TBPRD
-    EPwm?Regs.TBCTL.bit.HSPCLKDIV = ;
-    EPwm?Regs.TBCTL.bit.CLKDIV = ;
+    // MTRA
+    CpuSysRegs.PCLKCR2.bit.EPWM1 = 1;            // module clk EN
+    EPwm1Regs.TBCTL.bit.CTRMODE = 0b10;  // up-down count mode
+    EPwm1Regs.TBPRD = 2000;              // vres = 24mV = 2VDC/TBPRD = 48V/TBPRD
+    EPwm1Regs.TBCTL.bit.HSPCLKDIV = 0;   // no further division required to reduce fpwm
+    EPwm1Regs.TBCTL.bit.CLKDIV = 0;      // fpwm = fclk,pwm / (2*TBPRD*CLKDIV*HSPCLKDIV)
+    EPwm1Regs.CMPA.bit.CMPA = 340;
+    EPwm1Regs.AQCTLA.bit.CAU = 0b10;
+    EPwm1Regs.AQCTLA.bit.CAD = 0b01;
+
+    // MTRB
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO3 = 0;
+   GpioCtrlRegs.GPAMUX1.bit.GPIO3 = 1;
+   GpioCtrlRegs.GPAGMUX1.bit.GPIO4 = 0;
+   GpioCtrlRegs.GPAMUX1.bit.GPIO4 = 1;
+    CpuSysRegs.PCLKCR2.bit.EPWM2 = 1;            // module clk EN
+    asm(" NOP"); asm(" NOP");
+    EPwm2Regs.TBCTL.bit.CTRMODE = 0b10;
+    EPwm2Regs.TBPRD = 2000;
+    EPwm2Regs.TBCTL.bit.HSPCLKDIV = 0;
+    EPwm2Regs.TBCTL.bit.CLKDIV = 0;
+    EPwm2Regs.CMPA.bit.CMPA = 340;
+    EPwm2Regs.AQCTLA.bit.CAU = 0b01;
+    EPwm2Regs.AQCTLA.bit.CAD = 0b10;
+    // Enable clock connection to all pwm
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 1;
+
 
     // QEP Setup
-    GpioCtrlRegs.GP?GMUX?.bit.GPIO? = ;
-    GpioCtrlRegs.GP?MUX?.bit.GPIO? = ;
-    CpuSysRegs.PCLKCR4.bit.EQEP? = ?;  // QEP CLK EN
-    asm(" NOP"); asm(" NOP");
-    EQep?Regs.QPOSMAX = 0xFFFFFFFF;  // MAXIMUM COUNTER
-    EQep?Regs.QPOSINIT = ;  // counter EN & init
-    EQep?Regs.QEPCTL.bit.QPEN = ;
-    EQep?Regs.QEPCTL.bit.SWI = ;
+//    GpioCtrlRegs.GP?GMUX?.bit.GPIO? = ;
+//    GpioCtrlRegs.GP?MUX?.bit.GPIO? = ;
+//    CpuSysRegs.PCLKCR4.bit.EQEP? = ?;  // QEP CLK EN
+//    asm(" NOP"); asm(" NOP");
+//    EQep?Regs.QPOSMAX = 0xFFFFFFFF;  // MAXIMUM COUNTER
+//    EQep?Regs.QPOSINIT = ;  // counter EN & init
+//    EQep?Regs.QEPCTL.bit.QPEN = ;
+//    EQep?Regs.QEPCTL.bit.SWI = ;
 
     // Interrupt Timer (timer0, ftmr=1kHz)
     // from 200MHz to 1kHz
@@ -78,14 +107,17 @@ int main(void)
 
     // wait 500us at SYSCLK = 200MHz
     // (100000 cycles)
-    Uint32 j = 0;
-    for (j = 0; j < 1000; j++) {}
-    CpuTimer0Regs.TCR.bit.TSS = 0; WdRegs.WDCR.all = 0x28; EDIS; EINT;
+//    Uint32 j = 0;
+//    for (j = 0; j < 1000; j++) {}
+    CpuTimer0Regs.TCR.bit.TSS = 0;
+    WdRegs.WDCR.all = 0x28; EDIS; EINT;
     //===============================End Setup===============================
+
 
     while (1) {
         WdRegs.WDKEY.all = 0x55;
         WdRegs.WDKEY.all = 0xAA;
+        ct++;
     }
 }
 
@@ -94,12 +126,68 @@ int main(void)
 
 /** vcalcISR
  *
- * Recalculates desired vavg
+ * Controls PWM to create desired average voltage from 24VDC supply
+ * Computes next vavg from state machine:
+ *      +20V -> 0V -> -20V -> 0V -> +20V
  *
  * Timer0 interrupt
+ * ftmr0 = 1kHz
  *
  */
 interrupt void vcalcISR(void)
 {
-    vavg = 0;
+    // logic for switching state
+    // switch state every 200 clock triggers (1kHz -> 0.2s)
+    if (tmr0_count % 200 == 0) {
+        switch (state) {
+
+            // +20VDC out
+            case 0:
+                // 83% duty cycle from 24VDC in
+                vavg = 20;
+                // PWM2 forced low in prev state, so don't set here
+//                EPwm1Regs.CMPA.bit.CMPA = 340;  // 83% coverage of TBPRD=2000
+//                GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;
+                // update state
+                state = 1;
+                break;
+
+            // 0VDC
+            case 1:
+                // 0% duty cycle
+                vavg = 0;
+//                EPwm1Regs.CMPA.bit.CMPA = 0;  // 100% coverage of TBPRD=2000
+//                EPwm2Regs.CMPA.bit.CMPA = 0;  // 100% coverage of TBPRD=2000
+
+                // update state
+                state = 2;
+                break;
+
+            // -20VDC
+            case 2:
+                // 0% duty cycle
+                vavg = -20;
+                // PWM1 forced low in prev state, so don't set here
+//                EPwm1Regs.CMPA.bit.CMPA = 0;  // 83% coverage of TBPRD=2000
+//                EPwm2Regs.CMPA.bit.CMPA = 340;  // 83% coverage of TBPRD=2000
+
+                // update state
+                state = 3;
+                break;
+
+            // 0VDC
+            case 3:
+                // 0% duty cycle
+                vavg = 0;
+//                EPwm1Regs.CMPA.bit.CMPA = 0;  // 83% coverage of TBPRD=2000
+//                EPwm2Regs.CMPA.bit.CMPA = 0;  // 100% coverage of TBPRD=2000
+
+                // update state
+                state = 0;
+                break;
+        }
+    }
+
+    tmr0_count++;
+    PieCtrlRegs.PIEACK.all = M_INT1;
 }
