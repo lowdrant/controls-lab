d03f3a73602ee19c7a8cd8b4c0749890f88f9a70
Marion Anderson <lmanderson42@gmail.com>
2018-10-11 18:39:42 -0400

    Work on QEP and converted constants to macros

--- Lab5/Task1/main.c
+++ Lab5/Task1/main.c
@@ -9,14 +9,21 @@
  * Date: 2018-10-04
  */
 
-#include "F2837xD_device.h"
-#ifndef VIN
+#ifndef VIN            // System Input Voltage
 #define VIN 24
 #endif
+#ifndef PWM_MAX_COUNT  // PWM Maximum Count
+#define PWM_MAX_COUNT 2000
+#endif
 
-Uint16 state = 0;       // Vo state tracker
+#include "F2837xD_device.h"
+#include "math.h"
+
+Uint16 state = 0;       // Output voltage state tracker
 Uint32 tmr0_count = 0;  // software "clock divider" for incrementing states
-interrupt void vcalcISR(void);
+
+interrupt void TimerISR(void);  // timer0-based output voltage interrupt
+Uint16 pwmCMP(float);           // pwm compare value calculator
 
 
 int main(void)
@@ -48,11 +55,11 @@ int main(void)
     CpuSysRegs.PCLKCR2.bit.EPWM1 = 1;    // module clk EN
     asm(" NOP"); asm(" NOP");            // critical wait
     EPwm1Regs.TBCTL.bit.CTRMODE = 0b10;  // up-down count mode
-    EPwm1Regs.TBPRD = 2000;              // vres = 24mV = 2VDC/TBPRD = 48V/2000
+    EPwm1Regs.TBPRD = PWM_MAX_COUNT;     // vres = 24mV = 2VDC/TBPRD = 48V/2000
     EPwm1Regs.TBCTL.bit.HSPCLKDIV = 0;   // no further clock division needed
     EPwm1Regs.TBCTL.bit.CLKDIV = 0;      // fpwm = fclk,pwm / (2*TBPRD*CLKDIV*HSPCLKDIV)
     // pulse control | D~=0.9167 (20V from +/-24V pulses)
-    EPwm1Regs.CMPA.bit.CMPA = 166;       // D=0.9173 with maxval=2000
+    EPwm1Regs.CMPA.bit.CMPA = 167;       // D=0.9173 with maxval=2000
     EPwm1Regs.AQCTLA.bit.CAU = 0b10;     // 1A high on upcount
     EPwm1Regs.AQCTLA.bit.CAD = 0b01;     // 1A low on downcount
     EPwm1Regs.AQCTLB.bit.CAU = 0b01;     // 1B low on upcount
@@ -64,24 +71,23 @@ int main(void)
     CpuSysRegs.PCLKCR2.bit.EPWM2 = 1;  // clock setup
     asm(" NOP"); asm(" NOP");
     EPwm2Regs.TBCTL.bit.CTRMODE = 0b10;
-    EPwm2Regs.TBPRD = 2000;
+    EPwm2Regs.TBPRD = PWM_MAX_COUNT;
     EPwm2Regs.TBCTL.bit.HSPCLKDIV = 0;
     EPwm2Regs.TBCTL.bit.CLKDIV = 0;
-    EPwm2Regs.CMPA.bit.CMPA = 166;     // pulse control - opposite of PWM1
+    EPwm2Regs.CMPA.bit.CMPA = 167;     // pulse control - opposite of PWM1
     EPwm2Regs.AQCTLA.bit.CAU = 0b01;
     EPwm2Regs.AQCTLA.bit.CAD = 0b10;
     EPwm2Regs.AQCTLB.bit.CAU = 0b10;
     EPwm2Regs.AQCTLB.bit.CAD = 0b01;
 
-    // Boosterpack ENGATE
-    // force HIGH
+    // Boosterpack Enable
+    // pull ENGATE high
     GpioCtrlRegs.GPDGMUX2.bit.GPIO124 = 0;
     GpioCtrlRegs.GPDMUX2.bit.GPIO124 = 0;
     GpioCtrlRegs.GPDDIR.bit.GPIO124 = 1;
     GpioCtrlRegs.GPDPUD.bit.GPIO124 = 0;
     GpioDataRegs.GPDSET.bit.GPIO124 = 1;
-    // wait 1ms at 200MHz
-    Uint32 j;
+    Uint32 j;    // wait 1ms for activation (at 200MHz)
     for (j=0; j<200000; j++) {}
 
     // Timer0 Interrupt (ftmr=1kHz)
@@ -90,7 +96,7 @@ int main(void)
     CpuTimer0Regs.TCR.bit.TRB = 1;      // load timer division
     CpuTimer0Regs.TCR.bit.TIE = 1;      // Timer Interrupt Enable
     PieCtrlRegs.PIECTRL.bit.ENPIE = 1;
-    PieVectTable.TIMER0_INT = &vcalcISR;
+    PieVectTable.TIMER0_INT = &TimerISR;
     PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
     IER = 1;
 
@@ -105,11 +111,13 @@ int main(void)
 }
 
 
-/** vcalcISR
+/** TimerISR
+ *
+ * Creates desired average output voltage using PWM on DRV8305
  *
  * 2 Tasks:
  * - Update PWM to output desired vavg
- * - Increment state machine to update vavg
+ * - Increment state machine to change vavg
  *
  * Voltage Control
  * - Vo goes: +20V -> 0V -> -20V -> 0V -> +20V
@@ -136,20 +144,19 @@ int main(void)
  *
  * Remember to subtract 1 from the <state> to get the current state when
  * debugging.
- *
  */
-interrupt void vcalcISR(void)
+interrupt void TimerISR(void)
 {
     // logic for switching state
     // switch state every 200 timer0 interrupts (1kHz -> 0.2s)
-    if (tmr0_count % 2000 == 0) {
+    if (tmr0_count % 200 == 0) {
         switch (state) {
 
             // +20V
             // duty cycle of 91.7% to get vo=20V with current parameters
             case 0:
-                EPwm1Regs.CMPA.bit.CMPA = 166;
-                EPwm2Regs.CMPA.bit.CMPA = 166;
+                EPwm1Regs.CMPA.bit.CMPA = 167;
+                EPwm2Regs.CMPA.bit.CMPA = 167;
                 state = 1;  // update state
                 break;
 
@@ -164,8 +171,8 @@ interrupt void vcalcISR(void)
             // -20V
             // mirror of state 0 (same duty cycle, flipped output)
             case 2:
-                EPwm1Regs.CMPA.bit.CMPA = 1834;
-                EPwm2Regs.CMPA.bit.CMPA = 1834;
+                EPwm1Regs.CMPA.bit.CMPA = 1833;
+                EPwm2Regs.CMPA.bit.CMPA = 1833;
                 state = 3;
                 break;
 
@@ -181,4 +188,28 @@ interrupt void vcalcISR(void)
 
     tmr0_count++;
     PieCtrlRegs.PIEACK.all = M_INT1;
+}
+
+
+/** pwmCMP
+ *
+ * Computes PWM counter compare value from desired output voltage,
+ * assuming that the output is composed exclusively of +/-Vin DC pulses.
+ *
+ * System Setup:
+ * - PWM outputs exclusively +/-VIN DC pulses
+ * - PWM counter in up-down mode
+ * - VIN defined as global or macro
+ *
+ * Notes:
+ * - Duty Cycle = vo/2VIN + 0.5 (from the fact that output is +/- DC pulses)
+ * - Duty cycle inversely corresponds to counter compare value. This is
+ *   because an 80% duty cycle requires that the PWM trigger at 20% of the max
+ *   count to cover 80% of the counter cycle.
+ * - Uint16 because the PWM time-base counter is 16-bit (p.1827 of TRM)
+ */
+Uint16 pwmCMP(float vo)
+{
+    float d = (vo / VIN + 1) / 2;         // duty cycle
+    return round((1-d) * PWM_MAX_COUNT);  // convert to a counter value
 }
--- Lab5/Task2/main.c
+++ Lab5/Task2/main.c
@@ -9,20 +9,33 @@
  * Date: 2018-10-04
  */
 
-#include "F2837xD_device.h"
-#ifndef REVTICKS
-#define REVTICKS 360
+#ifndef VIN            // System Input Voltage
+#define VIN 24
+#endif
+#ifndef PWM_MAX_COUNT  // PWM Maximum Count
+#define PWM_MAX_COUNT 2000
+#endif
+#ifndef TICKS_PER_REV  // QEP Ticks per Revolution
+#define TICKS_PER_REV 1000  // float to avoid integer div
+#endif
+#ifndef LOG_LENGTH     // data points in data loggers
+#define LOG_LENGTH 2000
 #endif
 
-// data loggers
-vavg = 20;              // track desired avg voltage
-float vodes[2000];      // log desired average voltage
-float theta[2000];      // log actual angular position
+#include "F2837xD_device.h"
+#include "math.h"
 
-// interrupt parameters
-Uint16 state = 0;       // Vo state tracker
-Uint32 tmr0_count = 0;  // software clock divider for state machine
-interrupt void TimerISR(void);
+// Data Loggers
+Uint16 i;                     // index of data arrays
+int vo = 20;                  // track desired avg voltage
+float vo_log[LOG_LENGTH];     // log desired average voltage
+float theta_log[LOG_LENGTH];  // log actual angular position
+
+// State Machine Control
+Uint16 state = 0;       // Output voltage state tracker
+Uint32 tmr0_count = 0;  // software "clock divider" for incrementing states
+interrupt void TimerISR(void);  // timer0-based interrupt for lab i/o
+Uint16 pwmCMP(float);           // pwm compare value calculator
 
 
 int main(void)
@@ -51,40 +64,50 @@ int main(void)
     // PWM1 Setup: Booster Pack Phase A Control
     GpioCtrlRegs.GPAGMUX1.bit.GPIO0 = 0; GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1;
     GpioCtrlRegs.GPAGMUX1.bit.GPIO1 = 0; GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;
-    // clock
     CpuSysRegs.PCLKCR2.bit.EPWM1 = 1;    // module clk EN
     asm(" NOP"); asm(" NOP");            // critical wait
     EPwm1Regs.TBCTL.bit.CTRMODE = 0b10;  // up-down count mode
-    EPwm1Regs.TBPRD = 2000;              // vres = 24mV = 2VDC/TBPRD = 48V/2000
+    EPwm1Regs.TBPRD = PWM_MAX_COUNT;     // vres = 24mV = 2VDC/TBPRD = 48V/2000
     EPwm1Regs.TBCTL.bit.HSPCLKDIV = 0;   // no further clock division needed
     EPwm1Regs.TBCTL.bit.CLKDIV = 0;      // fpwm = fclk,pwm / (2*TBPRD*CLKDIV*HSPCLKDIV)
     // pulse control | D~=0.9167 (20V from +/-24V pulses)
-    EPwm1Regs.CMPA.bit.CMPA = 166;       // D=0.9173 with maxval=2000
+    EPwm1Regs.CMPA.bit.CMPA = 167;       // D=0.9173 with maxval=2000
     EPwm1Regs.AQCTLA.bit.CAU = 0b10;     // 1A high on upcount
     EPwm1Regs.AQCTLA.bit.CAD = 0b01;     // 1A low on downcount
     EPwm1Regs.AQCTLB.bit.CAU = 0b01;     // 1B low on upcount
     EPwm1Regs.AQCTLB.bit.CAD = 0b10;     // 1B high in downcount
 
     // PWM2 Setup: Booster Pack Phase B Control
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO2 = 0; GpioCtrlRegs.GPAMUX1.bit.GPIO2 = 1;
     GpioCtrlRegs.GPAGMUX1.bit.GPIO3 = 0; GpioCtrlRegs.GPAMUX1.bit.GPIO3 = 1;
-    GpioCtrlRegs.GPAGMUX1.bit.GPIO4 = 0; GpioCtrlRegs.GPAMUX1.bit.GPIO4 = 1;
     CpuSysRegs.PCLKCR2.bit.EPWM2 = 1;  // clock setup
     asm(" NOP"); asm(" NOP");
     EPwm2Regs.TBCTL.bit.CTRMODE = 0b10;
-    EPwm2Regs.TBPRD = 2000;
+    EPwm2Regs.TBPRD = PWM_MAX_COUNT;
     EPwm2Regs.TBCTL.bit.HSPCLKDIV = 0;
     EPwm2Regs.TBCTL.bit.CLKDIV = 0;
-    EPwm2Regs.CMPA.bit.CMPA = 166;     // pulse control - opposite of PWM1
+    EPwm2Regs.CMPA.bit.CMPA = 167;     // pulse control - opposite of PWM1
     EPwm2Regs.AQCTLA.bit.CAU = 0b01;
     EPwm2Regs.AQCTLA.bit.CAD = 0b10;
     EPwm2Regs.AQCTLB.bit.CAU = 0b10;
     EPwm2Regs.AQCTLB.bit.CAD = 0b01;
 
+    // Boosterpack Enable
+    // pull ENGATE high
+    GpioCtrlRegs.GPDGMUX2.bit.GPIO124 = 0;
+    GpioCtrlRegs.GPDMUX2.bit.GPIO124 = 0;
+    GpioCtrlRegs.GPDDIR.bit.GPIO124 = 1;
+    GpioCtrlRegs.GPDPUD.bit.GPIO124 = 0;
+    GpioDataRegs.GPDSET.bit.GPIO124 = 1;
+    Uint32 j;    // wait 1ms for activation (at 200MHz)
+    for (j=0; j<200000; j++) {}
+
     // QEP Setup
     GpioCtrlRegs.GPAGMUX2.bit.GPIO20 = 0; GpioCtrlRegs.GPAMUX2.bit.GPIO20 = 1;
     GpioCtrlRegs.GPAGMUX2.bit.GPIO21 = 0; GpioCtrlRegs.GPAMUX2.bit.GPIO21 = 1;
     GpioCtrlRegs.GPAGMUX2.bit.GPIO23 = 0; GpioCtrlRegs.GPAMUX2.bit.GPIO23 = 1;
-    CpuSysRegs.PCLKCR4.bit.EQEP1 = 1; asm(" NOP"); asm(" NOP");
+    CpuSysRegs.PCLKCR4.bit.EQEP1 = 1;
+    asm(" NOP"); asm(" NOP");
     EQep1Regs.QPOSMAX = 0xFFFFFFFF;  // MAXIMUM COUNTER!!
     EQep1Regs.QPOSINIT = 0;          // init value of 0
     EQep1Regs.QEPCTL.bit.QPEN = 1;   // enable qep1
@@ -110,13 +133,12 @@ int main(void)
     }
 }
 
-
 /** TimerISR
  *
- * 2 Tasks:
- * - Control voltage according to state machine
- * - Read position
- *
+ * Handles hardware I/O at 1kHz.
+ * - Controls output voltage through pwm
+ * - Increments voltage output state machine
+ * - Logs motor angular position and desired vout
  *
  * Voltage Control
  * - Vo goes: +20V -> 0V -> -20V -> 0V -> +20V
@@ -137,9 +159,9 @@ int main(void)
  *      PWM2A -> INHB
  *      PWM2B -> INLB
  *
- * Position Reading
- * - Read EQep1Regs.QPOSCNT to get how many ticks
- * - Convert ticks to angle
+ * QEP Interface
+ * - Read QPOSCNT
+ * - Convert to an angular position
  *
  * The switch-case body of the code looks a little weird/lopsided because
  * on each interrupt it reads the state, changes the PWM, and then increments
@@ -147,7 +169,6 @@ int main(void)
  *
  * Remember to subtract 1 from the <state> to get the current state when
  * debugging.
- *
  */
 interrupt void TimerISR(void)
 {
@@ -157,74 +178,53 @@ interrupt void TimerISR(void)
         switch (state) {
 
             // +20V
+            // duty cycle of 91.7% to get vo=20V with current parameters
             case 0:
-                EPwm1Regs.AQCTLA.bit.CAU = 0b10;  // 1A high on upcount
-                EPwm1Regs.AQCTLA.bit.CAD = 0b01;  // 1A low on downcount
-                EPwm1Regs.AQCTLB.bit.CAU = 0b01;  // 1B low on upcount
-                EPwm1Regs.AQCTLB.bit.CAD = 0b10;  // 1B high in downcount
-                EPwm2Regs.AQCTLA.bit.CAU = 0b01;  // 2A low on upcount
-                EPwm2Regs.AQCTLA.bit.CAD = 0b10;  // 2A high on downcount
-                EPwm2Regs.AQCTLB.bit.CAU = 0b10;  // 2B high on upcount
-                EPwm2Regs.AQCTLB.bit.CAD = 0b01;  // 2B low on downcount
-
-                vavg = 20;
+                EPwm1Regs.CMPA.bit.CMPA = 167;
+                EPwm2Regs.CMPA.bit.CMPA = 167;
+                vo = 20;
                 state = 1;  // update state
                 break;
 
             // 0V
+            // d=0.5 gives vavg=0
             case 1:
-                // force everything low for High-Z mode
-                EPwm1Regs.AQCTLA.bit.CAU = 1;
-                EPwm1Regs.AQCTLA.bit.CAD = 1;
-                EPwm1Regs.AQCTLB.bit.CAU = 1;
-                EPwm1Regs.AQCTLB.bit.CAD = 1;
-                EPwm2Regs.AQCTLA.bit.CAU = 1;
-                EPwm2Regs.AQCTLA.bit.CAD = 1;
-                EPwm2Regs.AQCTLB.bit.CAU = 1;
-                EPwm2Regs.AQCTLB.bit.CAD = 1;
-                vavg = 0;
+                EPwm1Regs.CMPA.bit.CMPA = 1000;
+                EPwm2Regs.CMPA.bit.CMPA = 1000;
+                vo = 0;
                 state = 2;
                 break;
 
-            // -20VDC
+            // -20V
+            // mirror of state 0 (same duty cycle, flipped output)
             case 2:
-                // mirror of case 0
-                EPwm1Regs.AQCTLA.bit.CAU = 0b01;
-                EPwm1Regs.AQCTLA.bit.CAD = 0b10;
-                EPwm1Regs.AQCTLB.bit.CAU = 0b10;
-                EPwm1Regs.AQCTLB.bit.CAD = 0b01;
-                EPwm2Regs.AQCTLA.bit.CAU = 0b10;
-                EPwm2Regs.AQCTLA.bit.CAD = 0b01;
-                EPwm2Regs.AQCTLB.bit.CAU = 0b01;
-                EPwm2Regs.AQCTLB.bit.CAD = 0b10;
-                vavg = -20;
+                EPwm1Regs.CMPA.bit.CMPA = 1833;
+                EPwm2Regs.CMPA.bit.CMPA = 1833;
+                vo = -20;
                 state = 3;
                 break;
 
             // 0VDC
+            // d=0.5 gives vavg=0
             case 3:
-                // force everything low for High-Z mode
-                EPwm1Regs.AQCTLA.bit.CAU = 1;
-                EPwm1Regs.AQCTLA.bit.CAD = 1;
-                EPwm1Regs.AQCTLB.bit.CAU = 1;
-                EPwm1Regs.AQCTLB.bit.CAD = 1;
-                EPwm2Regs.AQCTLA.bit.CAU = 1;
-                EPwm2Regs.AQCTLA.bit.CAD = 1;
-                EPwm2Regs.AQCTLB.bit.CAU = 1;
-                EPwm2Regs.AQCTLB.bit.CAD = 1;
-                vavg = 0;
+                EPwm1Regs.CMPA.bit.CMPA = 1000;
+                EPwm2Regs.CMPA.bit.CMPA = 1000;
+                vo = 0;
                 state = 0;
                 break;
         }
     }
 
-    // Log data
-    // use tmr0_count as the indexer for data
-    if (tmr0_count < 2000) {
-        vodes[tmr0_count] = vavg;
-        theta[tmr0_count] = EQep1Regs.QPOSCNT / (float) REVTICKS;l
+    // QEP reading & logging
+    long ticks = EQep1Regs.QPOSCNT;  // convert Uint32 to int32
+    float theta = (float) ticks / TICKS_PER_REV;  // TICKS_PER_REV is float
+    if (i < LOG_LENGTH) {
+        vo_log[i] = vo;
+        theta_log[i] = theta;
+        i++;
     }
 
+    // Exiting interrupt
     tmr0_count++;
     PieCtrlRegs.PIEACK.all = M_INT1;
 }
