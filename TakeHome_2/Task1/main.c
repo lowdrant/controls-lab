5575f958a2d72f9a9b7ca56de168db866ea73356
Marion Anderson <lmanderson42@gmail.com>
2018-10-29 12:50:46 -0400

    TH2 now at 9600.6 baud

--- TakeHome_2/Task1/main.c
+++ TakeHome_2/Task1/main.c
@@ -23,17 +23,17 @@ int main(void)
     DINT; EALLOW; SysCtrlRegs.SCSR = 0b10; SysCtrlRegs.WDCR = 0x68;
 
     // Clock setup
-    // System (10MHz ref -> 30MHz for easy serial clock setting)
+    // System (10MHz ref -> 50MHz for accurate 9600baud)
     SysCtrlRegs.PLLSTS.bit.DIVSEL = 0;            // PLL bypass
     SysCtrlRegs.PLLSTS.bit.MCLKOFF = 1;           // ignore failures for now
-    SysCtrlRegs.PLLCR.bit.DIV = 3;                // multiplier (3)
+    SysCtrlRegs.PLLCR.bit.DIV = 5;                // multiplier (5)
     while(SysCtrlRegs.PLLSTS.bit.PLLLOCKS != 1);  // wait for clock to latch
     SysCtrlRegs.PLLSTS.bit.MCLKOFF = 0;           // detect failures
     SysCtrlRegs.PLLSTS.bit.DIVSEL = 3;            // divider (1)
     // Peripheral/SCI
-    SysCtrlRegs.LOSPCP.bit.LSPCLK = 1;      // 15MHz sciclk (1/2 sysclk)
+    SysCtrlRegs.LOSPCP.bit.LSPCLK = 0;            // 17.5MHz (1x sysclk)
     asm(" NOP"); asm(" NOP");
-    SysCtrlRegs.PCLKCR0.bit.SCIAENCLK = 1;  // enable serial clock
+    SysCtrlRegs.PCLKCR0.bit.SCIAENCLK = 1;        // enable serial clock
     asm(" NOP"); asm(" NOP");
 
     // LED setup
@@ -54,12 +54,16 @@ int main(void)
     GpioDataRegs.GPASET.bit.GPIO2 = 1;
     GpioDataRegs.GPASET.bit.GPIO3 = 1;
 
-    // Serial setup
-    SciaRegs.SCIHBAUD = 0b11;               // 2400 baud (BRR=780)
-    SciaRegs.SCILBAUD = 0b00001100;
+    // Serial GPIO (just mux to serial)
+    GpioCtrlRegs.GPAMUX2.bit.GPIO28 = 1;
+    GpioCtrlRegs.GPAMUX2.bit.GPIO29 = 1;
+
+    // Serial configuration
+    SciaRegs.SCIHBAUD = 0x2;                // 9600 baud (BRR=650)
+    SciaRegs.SCILBAUD = 0x8a;
     SciaRegs.SCICCR.bit.SCICHAR = 0b111;    // 8-bit serial packet (ASCII)
     SciaRegs.SCICTL1.bit.SWRESET = 1;       // disable serial reset
-    SciaRegs.SCICTL1.bit.TXENA = 1;         // transmit enable
+    SciaRegs.SCICTL1.bit.TXENA = 1;         // transmit enable001
     SciaRegs.SCICTL1.bit.RXENA = 1;         // recieve enable
 
     // Interrupt setup
@@ -73,7 +77,6 @@ int main(void)
     // ===============================End Setup================================
     while (1) {
         loop_count++;
-        SciaRegs.SCITXBUF = 'A';
         SysCtrlRegs.WDKEY = 0x55;
         SysCtrlRegs.WDKEY = 0xAA;
     }
@@ -87,25 +90,25 @@ interrupt void serialISR(void)
 
     // Read Serial Data
     cmd_char = SciaRegs.SCIRXBUF.bit.RXDT;  // store character
-//    SciaRegs.SCITXBUF = cmd_char;  // retransmit character
+    SciaRegs.SCITXBUF = cmd_char;  // retransmit character
 
     // Change Active LED
-//    GpioDataRegs.GPACLEAR.bit.GPIO0 = 1;  // LEDs off 1st for code brevity
-//    GpioDataRegs.GPACLEAR.bit.GPIO1 = 1;
-//    GpioDataRegs.GPACLEAR.bit.GPIO2 = 1;
-//    GpioDataRegs.GPACLEAR.bit.GPIO3 = 1;
+    GpioDataRegs.GPASET.bit.GPIO0 = 1;  // LEDs off 1st for code brevity
+    GpioDataRegs.GPASET.bit.GPIO1 = 1;
+    GpioDataRegs.GPASET.bit.GPIO2 = 1;
+    GpioDataRegs.GPASET.bit.GPIO3 = 1;
     switch (cmd_char) {                   // re-activate selected LED
     case '0':
-        GpioDataRegs.GPASET.bit.GPIO0 = 1;
+        GpioDataRegs.GPACLEAR.bit.GPIO0 = 1;
         break;
     case '1':
-        GpioDataRegs.GPASET.bit.GPIO1 = 1;
+        GpioDataRegs.GPACLEAR.bit.GPIO1 = 1;
         break;
     case '2':
-        GpioDataRegs.GPASET.bit.GPIO2 = 1;
+        GpioDataRegs.GPACLEAR.bit.GPIO2 = 1;
         break;
     case '3':
-        GpioDataRegs.GPASET.bit.GPIO3 = 1;
+        GpioDataRegs.GPACLEAR.bit.GPIO3 = 1;
         break;
     }
 
